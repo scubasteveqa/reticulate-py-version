bb73577e043a5afdab5219fbfcc396f06bd06c0b
scubasteveqa <154364741+scubasteveqa@users.noreply.github.com>
2024-12-05 10:03:38 -0800

    Update app.R

--- app.R
+++ app.R
@@ -1,47 +1,16 @@
 library(shiny)
 library(reticulate)
 
-# Define UI
 ui <- fluidPage(
-  # Application title
-  titlePanel("Shiny App with Python Version and Histogram"),
-  
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-    mainPanel(
-      # Display the Python version
-      textOutput("python_version"),
-      # Show the histogram
-      plotOutput("distPlot")
-    )
-  )
+  titlePanel("Python Version in Shiny App"),
+  verbatimTextOutput("python_version")
 )
 
-# Define server logic
 server <- function(input, output, session) {
-  # Render the Python version
-  output$python_version <- renderText({
-    python_version <- as.character(py_config()$version)
-    paste("Python version:", python_version)
-  })
-  
-  # Render the histogram
-  output$distPlot <- renderPlot({
-    # Generate bins based on input$bins from UI
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # Draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  output$python_version <- renderPrint({
+    python_version <- py_config()$version
+    cat("Python version:", python_version, "\n")
   })
 }
 
-# Run the application
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
