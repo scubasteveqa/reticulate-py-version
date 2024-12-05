69de711e3732fb4b241b6d0bbc56f7d1480934a6
scubasteveqa <154364741+scubasteveqa@users.noreply.github.com>
2024-12-05 10:51:41 -0800

    Update app.R

--- app.R
+++ app.R
@@ -1,16 +1,15 @@
 library(shiny)
 library(reticulate)
 
+python_version <- as.character(py_config()$version) # Compute version upfront
+
 ui <- fluidPage(
   titlePanel("Display the Python Version through reticulate"),
-  textOutput("python_version", inline = TRUE)  
+  span(paste("Python version:", python_version), id = "python_version")  # Render as static text
 )
 
 server <- function(input, output, session) {
-  output$python_version <- renderText({
-    python_version <- as.character(py_config()$version)
-    paste("Python version:", python_version)
-  })
+  # No server-side updates required
 }
 
 shinyApp(ui, server)
