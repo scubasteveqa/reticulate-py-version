2a4e2282cfdcef6169b6778c95ecdb8bc3386dd8
scubasteveqa <154364741+scubasteveqa@users.noreply.github.com>
2024-12-05 10:47:33 -0800

    Update app.R

--- app.R
+++ app.R
@@ -3,13 +3,13 @@ library(reticulate)
 
 ui <- fluidPage(
   titlePanel("Display the Python Version through reticulate"),
-  textOutput("python_version")  # Use textOutput for plain text
+  textOutput("python_version", inline = TRUE)  
 )
 
 server <- function(input, output, session) {
   output$python_version <- renderText({
     python_version <- as.character(py_config()$version)
-    paste("Python version:", python_version)  # Generate plain text output
+    paste("Python version:", python_version)
   })
 }
 
